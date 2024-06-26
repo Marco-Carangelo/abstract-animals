7ab83aa7ad723308f453ef8342a7784310337373
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-26 16:05:54 +0200

    add: Animal methods implemented

--- src/main/java/org/lessons/java/animals/Animal.java
+++ src/main/java/org/lessons/java/animals/Animal.java
@@ -1,5 +1,26 @@
 package org.lessons.java.animals;
 
 public abstract class Animal {
+	
+	
+	
+	
+	
+	
+	
+	//Class Methods
+	
+	public void animalSleep(){
+		
+		System.out.println("Zzz...");
+		
+	}
+	
+	//Class abstract Methods
+	
+	public abstract void animalVerse();
+	public abstract void animalEat();
+	
+	
 
 }
