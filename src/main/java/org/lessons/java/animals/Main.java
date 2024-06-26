4b1e7dca64244653abb3dc7c9066e7117beabf6f
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-26 16:32:07 +0200

    add: Main class

---
+++ src/main/java/org/lessons/java/animals/Main.java
@@ -0,0 +1,11 @@
+package org.lessons.java.animals;
+
+import java.util.Scanner;
+
+public class Main {
+
+	//Initialize a scanner to acquire date from user
+	
+	Scanner scanner = new Scanner(System.in);
+	
+}
