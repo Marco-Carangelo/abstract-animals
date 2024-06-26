82a331c62cbb65a7a925834c0436130e2fc984ae
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-26 17:22:07 +0200

    add: Animal class children houseSparrow, Eagle and Dolphin

---
+++ src/main/java/org/lessons/java/animals/Eagle.java
@@ -0,0 +1,22 @@
+package org.lessons.java.animals;
+
+public class Eagle extends Animal {
+
+	public Eagle(String animalFood, String animalSound) {
+		super(animalFood, animalSound);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void animalVerse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void animalEat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/org/lessons/java/animals/houseSparrow.java
@@ -0,0 +1,22 @@
+package org.lessons.java.animals;
+
+public class houseSparrow extends Animal {
+
+	public houseSparrow(String animalFood, String animalSound) {
+		super(animalFood, animalSound);
+		
+	}
+
+	@Override
+	public void animalVerse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void animalEat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
