2cdbec045071becd427a24ec874da3fdb2938751
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-26 16:29:08 +0200

    Add: animalSound field added to Animal class

--- src/main/java/org/lessons/java/animals/Animal.java
+++ src/main/java/org/lessons/java/animals/Animal.java
@@ -5,18 +5,27 @@ public abstract class Animal {
 	//Class fields
 	
 	private String animalFood;
+	private String animalSound;
 	
 	//Constructor method
 	
-
-	public Animal(String animalFood) {
+	public Animal(String animalFood, String animalSound) {
 		
 		this.animalFood = animalFood;
+		this.animalSound = animalSound;
 		
 	}
 	
 	//Getters and Setters
 	
+	public String getAnimalSound() {
+		return animalSound;
+	}
+
+	public void setAnimalSound(String animalSound) {
+		this.animalSound = animalSound;
+	}
+
 	public String getAnimalFood() {
 		return animalFood;
 	}
