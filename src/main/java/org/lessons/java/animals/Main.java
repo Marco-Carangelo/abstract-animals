d499753d4329c91c82e3a8848da5a70d4b4076a2
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-26 16:48:56 +0200

    add: trial of Animal and Dog fields and methods in main

--- src/main/java/org/lessons/java/animals/Main.java
+++ src/main/java/org/lessons/java/animals/Main.java
@@ -3,9 +3,30 @@ package org.lessons.java.animals;
 import java.util.Scanner;
 
 public class Main {
+	
+	public static void main(String[] args) {
 
-	//Initialize a scanner to acquire date from user
+		//Initialize a scanner to acquire date from user
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//Define two variables to acquire animals info from user
+		
+		System.out.println("Insert animal food: ");
+		String userFood =  scanner.nextLine();
+		
+		System.out.println("Insert animal verse: ");
+		String userVerse=  scanner.nextLine();
+		
+		//Create an instance of Dog class
+		
+		Dog doggo = new Dog(userFood , userVerse );
+		
+		//Try Dog and Animal methods
+		
+		doggo.animalSleep();
+		doggo.animalEat();
+		doggo.animalVerse();
 	
-	Scanner scanner = new Scanner(System.in);
-	
+	}
 }
