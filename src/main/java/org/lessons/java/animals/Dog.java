52b54679483d86dc64f9b674d832356f74a31bb1
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-26 16:17:14 +0200

    add: Dog class, child of the Animal class

---
+++ src/main/java/org/lessons/java/animals/Dog.java
@@ -0,0 +1,24 @@
+package org.lessons.java.animals;
+
+public class Dog extends Animal{
+	
+	
+
+	public Dog(String animalFood) {
+		super(animalFood);
+		
+	}
+
+	@Override
+	public void animalVerse() {
+		
+		
+	}
+
+	@Override
+	public void animalEat() {
+		
+		
+	}
+
+}
