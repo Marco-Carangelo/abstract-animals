18d8102159755d6dfb7e990061c0334d324bbcec
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-26 16:29:49 +0200

    add: animalVerse() method implemented in Dog class

--- src/main/java/org/lessons/java/animals/Dog.java
+++ src/main/java/org/lessons/java/animals/Dog.java
@@ -4,15 +4,15 @@ public class Dog extends Animal{
 	
 	
 
-	public Dog(String animalFood) {
-		super(animalFood);
+	public Dog(String animalFood,String animalSound) {
+		super(animalFood, animalSound);
 		
 	}
 
 	@Override
 	public void animalVerse() {
 		
-		
+		System.out.println("This animal verse is : " + getAnimalSound());
 		
 	}
 
